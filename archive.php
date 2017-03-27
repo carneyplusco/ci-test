fe30baee99764b33e5dc8872c11855eb171e3c9b
kulas <nkulas@gmail.com>
2017-03-27 16:36:31 -0400

    Removed anchor from cat heading

--- archive.php
+++ archive.php
@@ -6,7 +6,7 @@
 
 <section class="page-header">
   <span class="section-number"><?= $category_number ?>.</span>
-  <h1 class="page-header__title underline"><?php the_category(' ') ?></h1>
+  <h1 class="page-header__title underline"><?php single_cat_title(' ') ?></h1>
   <a class="page-header__back-button" href="<?= Extras\back_link() ?>" aria-label="Go back to previous page" tabindex="0">Back</a>
 </section>
 
