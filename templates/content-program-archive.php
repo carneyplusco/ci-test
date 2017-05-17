abbf6a653acf11ca96bf4054cd59ed723e1a54b1
Jesse Hixson <hixsonj@gmail.com>
2017-05-17 14:39:01 -0400

    Adds numbering to programs pages/subpages. Adjusts margins to account for 3rd level numbering

--- templates/content-program-archive.php
+++ templates/content-program-archive.php
@@ -1,4 +1,20 @@
+<?php
+use Roots\Sage\Extras;
+$menu_number = Extras\menu_number('Home Nav', 'Programs');
+$args = array(
+  'post_type'   => 'page',
+  'post_parent' => $post->post_parent,
+  'post_status' => 'publish',
+  'orderby'     => 'menu_order',
+  'order'       => 'asc'
+);
+$program_categories = get_posts($args);
+$index = array_search($post, $program_categories);
+$post_index = $index !== FALSE ? $index + 1: 1;
+?>
+
 <section class="page-header">
+<span class="section-number"><?= $menu_number .'.'. $post_index ?></span>
   <h2 class="page-header__title underline"><span class="page-title"><?php the_title() ?></a></h2>
 </section>
 
@@ -29,6 +45,7 @@
 
     <?php if(count($archive_posts)): ?>
       <?php foreach($archive_posts as $post): setup_postdata($post); ?>
+        <span class="article-list__section-number"><?= $menu_number .'.'. $post_index .'.'. $post_count++ ?></span>
         <article class="article-item" vocab="http://schema.org/" typeof="NewsArticle">
           <h3 class="article-item__title" property="headline"><a href="<?= get_the_permalink() ?>"><?php the_title(); ?></a></h3>
           <h4 class="article-item__date" aria-label="<?= the_time("F jS, Y") ?>">
